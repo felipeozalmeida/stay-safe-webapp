3568455e95a9deecc1748d344eab3334129d1c95
Felipe Almeida <felipeortalmeida@gmail.com>
2020-06-30 19:32:23 -0300

    Added material ui

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+function App() {
+  return (
+    <>
+      <Button variant="contained" color="primary">
+        Click me
+      </Button>
+    </>
+  );
+}
+
+export default App;
