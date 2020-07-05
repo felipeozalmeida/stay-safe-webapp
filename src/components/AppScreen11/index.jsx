171dfcf69bc0ad47b99d880f2e28426c009ac4ec
Felipe Almeida <felipeortalmeida@gmail.com>
2020-07-04 22:21:06 -0300

    Bootstrapped 11th screen

--- src/components/AppScreen11/index.jsx
+++ src/components/AppScreen11/index.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import { Box, Typography } from '@material-ui/core'
+import logo from '../../images/logo.png'
 
 const AppScreen11 = () => {
+
+  const symptoms = [
+    'Sintoma A',
+    'Sintoma B',
+    'Sintoma C',
+    'Sintoma D',
+    'Sintoma E',
+  ]
+
   return (
     <>
-      <h1>AppScreen11</h1>
+      <Box
+        display="flex"
+        alignItems="start"
+        justifyContent="center"
+        mt={2}
+        mb={4}
+      >
+        <img src={logo} alt="Logo do Stay Safe Webapp." />
+      </Box>
+      <Box mb={4}>
+        <Typography variant="h5">
+          Resumo semanal
+        </Typography>
+        <Typography>
+          Graph
+        </Typography>
+      </Box>
+      {symptoms.map((symptom, index) => (
+        <Box mb={4} key={index}>
+          <Typography variant="h5" gutterBottom>
+            {symptom}
+          </Typography>
+          <Typography>
+            Graph
+          </Typography>
+        </Box>
+      ))}
     </>
   )
 }
