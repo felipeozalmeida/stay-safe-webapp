bcb5740ad8ec0774857d80b1cf558fd6ecb69c17
Felipe Almeida <felipeortalmeida@gmail.com>
2020-07-06 21:55:05 -0300

    Finished basic level of AppScreen11

--- src/components/AppScreen11/index.jsx
+++ src/components/AppScreen11/index.jsx
@@ -1,15 +1,36 @@
 import React from "react";
-import { Box, Typography } from "@material-ui/core";
+import {
+  BottomNavigation,
+  BottomNavigationAction,
+  Box,
+  Button,
+  Typography,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  HomeOutlined as HomeOutlinedIcon,
+  PersonOutlined as PersonOutlinedIcon,
+  NotificationsOutlined as NotificationsOutlinedIcon,
+} from "@material-ui/icons";
+import { Alert } from "@material-ui/lab";
 import logo from "../../images/logo.png";
+import happyFace from "../../images/happyFace.png";
+
+const useStyles = makeStyles((theme) => ({
+  alert: {
+    backgroundColor: theme.palette.primary.main,
+    flexWrap: "wrap",
+  },
+  bottomNavigation: {
+    bottom: 0,
+    left: 0,
+    position: "fixed",
+    width: "100%",
+  },
+}));
 
 const AppScreen11 = () => {
-  const symptoms = [
-    "Sintoma A",
-    "Sintoma B",
-    "Sintoma C",
-    "Sintoma D",
-    "Sintoma E",
-  ];
+  const classes = useStyles();
 
   return (
     <>
@@ -23,17 +44,40 @@ const AppScreen11 = () => {
         <img src={logo} alt="Logo do Stay Safe Webapp." />
       </Box>
       <Box mb={4}>
-        <Typography variant="h5">Resumo semanal</Typography>
-        <Typography>Graph</Typography>
+        <Alert
+          variant="filled"
+          severity="info"
+          icon={false}
+          action={
+            <Button color="inherit" size="small">
+              Clique aqui
+            </Button>
+          }
+          className={classes.alert}
+        >
+          <Box fontWeight="fontWeightBold">Responda seu questionário!</Box>
+        </Alert>
       </Box>
-      {symptoms.map((symptom, index) => (
-        <Box mb={4} key={index}>
-          <Typography variant="h5" gutterBottom>
-            {symptom}
-          </Typography>
-          <Typography>Graph</Typography>
-        </Box>
-      ))}
+      <Box display="flex" alignItems="start" justifyContent="center" mb={4}>
+        <img src={happyFace} alt="Ilustração de um rosto feliz." />
+      </Box>
+      <Typography align="center">
+        Parabéns, continue se prevenindo e lembre-se de sempre lavar suas mãos.
+      </Typography>
+      <BottomNavigation className={classes.bottomNavigation}>
+        <BottomNavigationAction
+          label="Home"
+          icon={<HomeOutlinedIcon color="primary" fontSize="large" />}
+        />
+        <BottomNavigationAction
+          label="Perfil"
+          icon={<PersonOutlinedIcon color="primary" fontSize="large" />}
+        />
+        <BottomNavigationAction
+          label="Notificações"
+          icon={<NotificationsOutlinedIcon color="primary" fontSize="large" />}
+        />
+      </BottomNavigation>
     </>
   );
 };
